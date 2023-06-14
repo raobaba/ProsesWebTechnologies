9d90566e1e37016c51c8475930eb255c3d272eb0
raobaba <raorajan9576@gmail.com>
2023-06-14 14:38:23 +0530

    todo completed

--- frontend/src/Components/Details.js
+++ frontend/src/Components/Details.js
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { fetchData, deleteData, updateData } from '../api';
 
 export default function Details() {
   const [data, setData] = useState([]);
+  const [editIndex, setEditIndex] = useState(-1);
+  const [updatedData, setUpdatedData] = useState({
+    username: '',
+    mobile: '',
+    email: '',
+    address: ''
+  });
 
   useEffect(() => {
-    fetchData();
+    fetchDataFromAPI();
   }, []);
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get('http://localhost:8000/getAll');
-      setData(response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+  const fetchDataFromAPI = async () => {
+    const newData = await fetchData();
+    setData(newData);
   };
 
   const handleComplete = (index) => {
@@ -27,20 +30,107 @@ export default function Details() {
       return newData;
     });
   };
+
+  const handleDelete = async (id) => {
+    try {
+      await deleteData(id);
+      setData((prevState) => prevState.filter(item => item._id !== id));
+    } catch (error) {
+      console.error('Error deleting data:', error);
+    }
+  };
+
+  const handleEdit = (index) => {
+    setEditIndex(index);
+    setUpdatedData({
+      username: data[index].username,
+      mobile: data[index].mobile,
+      email: data[index].email,
+      address: data[index].address
+    });
+  };
+
+  const handleUpdate = async (index) => {
+    try {
+      await updateData(data[index]._id, updatedData);
+
+      setData((prevState) => {
+        const newData = [...prevState];
+        newData[index] = {
+          ...newData[index],
+          username: updatedData.username,
+          mobile: updatedData.mobile,
+          email: updatedData.email,
+          address: updatedData.address
+        };
+        return newData;
+      });
+
+      setEditIndex(-1);
+    } catch (error) {
+      console.error('Error updating data:', error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUpdatedData((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
   return (
     <div className="details">
       {data.map((item, index) => (
         <div className={`input-data`} key={index}>
           <div className="task">
-            <p className={item.completed ? 'completed-text' : ''}>{item.username}</p>
-            <p className={item.completed ? 'completed-text' : ''}>{item.mobile}</p>
-            <p className={item.completed ? 'completed-text' : ''}>{item.email}</p>
-            <p className={item.completed ? 'completed-text' : ''}>{item.address}</p>
+            {editIndex === index ? (
+              <>
+                <input
+                  type="text"
+                  name="username"
+                  value={updatedData.username}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="text"
+                  name="mobile"
+                  value={updatedData.mobile}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="text"
+                  name="email"
+                  value={updatedData.email}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="text"
+                  name="address"
+                  value={updatedData.address}
+                  onChange={handleInputChange}
+                />
+              </>
+            ) : (
+              <>
+                <p className={item.completed ? 'completed-text' : ''}>{item.username}</p>
+                <p className={item.completed ? 'completed-text' : ''}>{item.mobile}</p>
+                <p className={item.completed ? 'completed-text' : ''}>{item.email}</p>
+                <p className={item.completed ? 'completed-text' : ''}>{item.address}</p>
+              </>
+            )}
           </div>
           <div className="custom-button">
-            <button onClick={() => handleComplete(index)}> {item.completed ? 'Completed' : 'Not Complete'}</button>
-            <button>Edit</button>
-            <button>Delete</button>
+          <button onClick={() => handleComplete(index)}>
+              {item.completed ? 'Completed' : 'Not Complete'}
+            </button>
+            {editIndex === index ? (
+              <button onClick={() => handleUpdate(index)}>Update</button>
+            ) : (
+              <button onClick={() => handleEdit(index)}>Edit</button>
+            )}
+            <button onClick={() => handleDelete(item._id)}>Delete</button>
           </div>
         </div>
       ))}
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -10,3 +10,46 @@ export const handleSubmit = async (formData) => {
     console.error('Failed to add todo', error);
   }
 };
+export const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/getAll');
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return [];
+    }
+  };
+
+// api.js
+export const deleteData = async (id) => {
+    const response = await fetch(`http://localhost:8000/delete/${id}`, {
+      method: 'DELETE',
+    });
+    if (!response.ok) {
+      throw new Error('Error deleting data');
+    }
+    // Optionally, you can return the response data if needed
+    const data = await response.json();
+    return data;
+  };
+  
+  export const updateData = async (id, updatedData) => {
+    try {
+      const response = await fetch(`http://localhost:8000/edit/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedData)
+      });
+      
+      if (!response.ok) {
+        throw new Error('Error updating data');
+      }
+      
+      const updatedItem = await response.json();
+      return updatedItem;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
